cab4c4ee005c8f79932b55956a08b510b4e253a8
SteamShon <shom83@gmail.com>
2012-06-25 20:25:20 +0900

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Railstutorial::Application.routes.draw do
-  get "static_pages/home"
-  get "static_pages/help"
-  get "static_pages/about"
+  get "users/new"
+
+  match '/help', to: 'static_pages#help'
+  match '/about', to: 'static_pages#about'
+  match '/contact', to: 'static_pages#contact'
+  match '/signup', to: 'users#new'
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -52,7 +55,7 @@ Railstutorial::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   # root :to => 'welcome#index'
-
+  root to: 'static_pages#home'
   # See how all your routes lay out with "rake routes"
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.

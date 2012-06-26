0e7ae5b875aea7187d10a527386ca0a3d2591643
SteamShon <shom83@gmail.com>
2012-06-26 09:25:52 +0900

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Railstutorial::Application.routes.draw do
-  get "users/new"
-
+  resources :users
+  match '/signup', to: 'users#new'
   match '/help', to: 'static_pages#help'
   match '/about', to: 'static_pages#about'
   match '/contact', to: 'static_pages#contact'
---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    name "doyoung"
+    email "doyoung@example.com"
+    password "password"
+    password_confirmation "password"
+  end
+end
